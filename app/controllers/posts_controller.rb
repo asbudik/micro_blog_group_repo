9ab048854adeba249aed5a80b4ea48a5d0212407
joemeers82 <joemeers82@gmail.com>
2014-08-23 00:48:04 -0700

    skltn_post_cntrl

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+
+	def index
+	end
+
+	def create
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+	def
 end
