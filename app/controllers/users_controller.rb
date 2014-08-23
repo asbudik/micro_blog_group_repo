fd55bb1e11589a115d0f49b44bc4e0563e1d4956
alexshev91 <alexshev91@yahoo.com>
2014-08-23 16:42:50 -0700

    Full CRUD for USERS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,51 @@
 class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+
+	def new
+		@user = User.new
+	end
+	def create
+		new_user = params[:user].permit(:email,:first_name,:last_name,:image_url)
+		User.create(new_user)
+		redirect_to users_path
+	end
+
+
+
+
+	def show
+		id = params[:id]
+		@user = User.find(id)
+		render :show
+	end
+
+
+  def edit
+    id = params[:id]
+    @user = User.find(id)
+    render :edit
+  end
+  def update
+    user_id = params[:id]
+    user = User.find(user_id)
+    
+    updated_params = params.require(:user).permit(:email,:first_name,:last_name,:image_url)
+    user.update_attributes(updated_params)
+    redirect_to user
+  end
+
+
+  def destroy
+    id = params[:id]
+    user = User.find(id)
+    user.destroy
+    redirect_to "/users"
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
 
+  # routes for users
+  resources :users do
+  end
+
 end
