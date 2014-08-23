887608497acd23d2e4e320fcc6977cf11fb1588d
alexshev91 <alexshev91@yahoo.com>
2014-08-22 23:19:28 -0700

    added seeds,did not test -Alex

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,79 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# create 3 users
+user1 = User.create({email: "yodathemasta@lightforce.co", first_name: "Yoda", last_name: "Greeny",image_url:"/yoda/img.png"})
+user2 = User.create({email: "luker@lightforce.co", first_name: "Luke", last_name: "Sky_walker",image_url:"/luke/img.png"})
+user3 = User.create({email: "darkOG@darkforce.co", first_name: "Darth", last_name: "Vader",image_url:"darth/img/png"})
+
+# add some pages to user1 (Yoda)
+user1.pages.create({name: "hmmm", content: "you must unlearn what you have learned"});
+user1.pages.create({name: "Oh wow", content: "jedi's rock"});
+# add some pages to user2 (luke)
+user2.pages.create({name: "LukeAboutMe", content: "i know the force"});
+user2.pages.create({name: "LukeCOntact", content: "use the force to find me bro"})
+# add a page to user3 (Darth)
+user3.pages.create({name: "DeathStarResorts", content: "Please visit our family business!"});
+
+#############################
+# create 4 posts
+post1 = Post.create({title: "the force using for dummies", body: "Coming soon by YODA"})
+post2 = Post.create({title: "lightsaber color chart", body: "chopping off limbs with style"})
+post3 = Post.create({title: "Help! My dad is missing", body: "Help me find my dad =("})
+post4 = Post.create({title: "StromTrooper discipline pamphlet", body: "Pushup reps as a punishiment"})
+
+# add 2 posts to user1 (yoda)
+user1.posts << post1
+user1.posts << post2
+# add 1 post to user2 (luke)
+user2.posts << post3
+# add 1 post to user 3 (darth)
+user3.posts << post4
+
+############################
+# create 3 tags
+tag1 = Tag.create({name: "#Master"})
+tag2 = Tag.create({name: "#Padavan"})
+tag3 = Tag.create({name: "#DarkSide"})
+
+# add tag1 to posts 1 and 2.Then add it to user1(yoda's)
+post1.tags << tag1
+post2.tags << tag1
+user1.tags << tag1
+# add tag2 to post3 and user2 (luke's)
+post3.tags << tag2
+user2.tags << tag2
+# add tag3 to post4 and user3 (luke's)
+post4.tags << tag3
+user3.tags << tag3
+
+############################
+#create 4 comments
+comment1 = Comment.create({content: "ty master, very helpful --the green girl from the front row"})
+comment2 = Comment.create({content: "sorry to hear that, man =( --JawaTheRobotSteeler"})
+comment3 = Comment.create({content: "I object! You could use some pushups yourself! --TrooperTheLazy"})
+comment4 = Comment.create({content: "I luv pushups!!! Yeeeeeah!!! --TrooperTheLifter"})
+
+#add comment1 to post1
+post1.comments << comment1
+#add comment2 to post2
+post2.comments << comment2
+#add comment3 and comment4 to post4
+post4.comments << comment3
+post4.comments << comment4
+
+############################
+# create 3 comments-on-comments
+comment1ONE = Comment.create({content: "yeah i agree"})
+comment1TWO = Comment.create({content: "the master should learn some grammar"})
+comment4ONE = Comment.create({content: "Yeah man! That's the spirit! --Darth"})
+
+#add comment1ONE and comment1TWO to comment1
+comment1.comments << comment1ONE
+comment1.comments << comment1TWO
+#add comment4ONE to comment4
+comment4.comments << comment4ONE
+
+
+
+
+
+
+
