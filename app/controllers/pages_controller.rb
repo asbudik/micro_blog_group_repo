db52589de83e2c24f5eb022fc0f021caa452e7ef
Adam Budik <asbudik@live.com>
2014-08-23 16:22:27 -0700

    add posts and pages controller skeletons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,21 @@
 class PagesController < ApplicationController
+
+def create
+end
+
+def new
+end
+
+def edit
+end
+
+def update
+end
+
+def show
+end
+
+def destroy
+end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,35 @@
 class PostsController < ApplicationController
+
+def index
+@all_posts =Post.all
+end
+
+def create
+post_data = params[:post].permit(:title, :body)
+end
+
+def new
+end
+
+def edit
+@post= Post.find_by_id(params[:id])
+end
+
+def update
+id_data=params[:id]
+post_data=params[:post].permit(:title,:body)
+end
+
+def show
+end
+
+def destroy
+post=Post.find_by_id(params[:id])
+if post
+post.destroy
+end
+redirect_to '/'
+end
+
+
 end
