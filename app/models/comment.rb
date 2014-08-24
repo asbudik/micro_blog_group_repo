b1ecd8f036fa598b08afc4f8f072c6ebf9bb2fc5
Adam Budik <asbudik@live.com>
2014-08-23 23:27:54 -0700

    validation regex

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   has_many :comments, :as => :commentable
+
+  validates :content, length: {:minimum => 2}, length: {:maximum => 30}, presence: true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,7 @@ class Tag < ActiveRecord::Base
   has_many :posttags
   has_many :posts, :through => :posttags
   has_many :users, :through => :posts
+
+  validates :name, presence: true, length: {:minimum => 3}, length: {:maximum => 12}
+  validates_format_of :name, :with => /\A+[a-zA-Z]*\z/
 end
