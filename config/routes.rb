dfe82539d5ebf25784cb7ee43084193cb4a1d8a6
Adam Budik <asbudik@live.com>
2014-08-23 18:32:54 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
 
+  get '/', to: 'sites#index'
+  get '/about_us', to: 'sites#show'
+
   # routes for users
   resources :users do
     resources :pages
-    resources :posts
+    resources :posts do
+      post '/comments', to: "comments#create"
+    end
   end
 
+  get '/users/:user_id/tags/:tag_id', to: 'tagusers#show'
+  get '/tags', to: 'tags#index'
+  get '/tags/:id', to: 'tags#show'
 end
