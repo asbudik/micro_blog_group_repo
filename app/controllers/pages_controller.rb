62d9a6b73d4ef2cb1b96a3b6765c8123f79bb45d
Adam Budik <asbudik@live.com>
2014-08-23 19:39:42 -0700

    Sites, Users, Pages controllers complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,53 @@
 class PagesController < ApplicationController
 
-def create
-end
+  def create
+    find_user_pages
 
-def new
-end
+    new_page = params.require(:page).permit(:name, :content)
+    @page = @user.pages.create(new_page)
 
-def edit
-end
+    redirect_to [@user, @page]
+  end
 
-def update
-end
+  def new
+    find_user_pages
+    @page = @user.pages.new
+  end
 
-def show
-end
+  def edit
+    find_user_pages
 
-def destroy
-end
+    @page = @user.pages.find(params[:id])
+  end
 
+  def update
+    find_user_pages
+
+    new_update_page = params.require(:page).permit(:name, :content)
+    @page = @user.pages.find(params[:id])
+
+    @page.update_attributes(new_update_page)
+
+    redirect_to [@user, @page]
+  end
+
+  def show
+    find_user_pages
+    @page = @user.pages.find(params[:id])
+  end
+
+  def destroy
+    find_user_pages
+
+    @user.pages.find(params[:id]).destroy
+
+    redirect_to users_path(@user.id)
+  end
+
+  # find the specified user in order to render the pages
+  # that belong to that user
+  def find_user_pages
+    user_id = params[:user_id]
+    @user = User.find(user_id)
+  end
 end
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,2 +1,9 @@
 class SitesController < ApplicationController
+
+  def index
+    @top_posts = Post.all.limit(3).order(created_at: :desc)
+  end
+
+  def show
+  end
 end
