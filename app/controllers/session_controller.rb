6f024d8634345e1b2e1976bfd7f72e91d42201ed
rasbabbo <rasbabbo@yahoo.com>
2014-08-25 18:22:45 -0700

    cleaned up bad words in login view and session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -9,14 +9,15 @@ class SessionController < ApplicationController
 
 		if @user
 			session[:user_id] = @user.user_id
-			render text: "uuhh, you loggin in!"
+			render text: "your logged in!"
 		else
-			render text: "Nope you ain't logged in"
+			render text: "not logged in"
 		end
 	end
 
 	def destroy
 		session[:user_id] =nil
-		render text: "Man you logged out!"
+		render text: "logged out!"
 	end
+
 end
