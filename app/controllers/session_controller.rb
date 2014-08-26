b8213e11a0cf5dd093d7d9cf015c16ec3c51ddac
rasbabbo <rasbabbo@yahoo.com>
2014-08-25 20:30:49 -0700

    added a logout button

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -11,7 +11,7 @@ class SessionController < ApplicationController
 			session[:user_id] = @user.user_id
 			render text: "your logged in!"
 		else
-			render text: "not loggied in"
+			render text: "not logged in"
 		end
 	end
 
