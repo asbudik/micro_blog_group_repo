5d6860fa36a82f68bd3242f0a5c79897ceb87505
joemeers82 <joemeers82@gmail.com>
2014-08-26 14:12:44 -0700

    logout added

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,4 @@
-class SessionsController < ApplicationController
+class SessionController < ApplicationController
 	def new
 	end
 	def create
@@ -13,6 +13,7 @@ class SessionsController < ApplicationController
 	end
 	def destroy
 		session[:user_id]=nil
-		render text: "logged out"
+		# render text: "logged out"
+		redirect_to '/'
 	end
 end
