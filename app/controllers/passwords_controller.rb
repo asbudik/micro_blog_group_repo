9176d678d1aac27cd9862ae00d3fcae72df1aa6a
rasbabbo <rasbabbo@yahoo.com>
2014-08-27 19:52:13 -0700

    password reset added

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,6 +1,30 @@
 class PasswordsController < ApplicationController
 
 	def create
-		user = User.find_by
+		user = User.find_by_email(params[:email])
+
+		if user
+			user.set_password_reset
+			UserMailer.password_reset(user).deliver
+			redirect_to root_url
+		end
+
+		redirect_to login_url, notice: "Please respond to email immediatley"
+	end
+
+	def edit
+		@user = User.find_by_code(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+
+		if @user
+			update_password = params.require(:user).permit(:password, :password_confirmation)
+			@user.update_attributes(update_password)
+			@user.save
+				redirect_to user_path
+			end
+		end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140824201711) do
+ActiveRecord::Schema.define(version: 20140827065717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20140824201711) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "code"
+    t.datetime "expires_at"
   end
 
 end
